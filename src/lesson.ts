280bfbe0c19e8b84d0cbd4e4ed080235615be896
timivarga <90131750+timivarga@users.noreply.github.com>
2022-10-19 16:33:12 +0200

    kesz :)

--- src/lesson.ts
+++ src/lesson.ts
@@ -11,6 +11,7 @@ export default class Calculator {
      */
 
 
+
     /** 
      * 1. feladat:
      * Hozd létre a sum metódust.
@@ -19,6 +20,10 @@ export default class Calculator {
      * @param {number} b - a második szám.
      * @returns {number} a két paraméterként kapott szám összege.
      */
+
+    sum(a: number, b:number){
+        return a+b;
+    }
 
 
     /** 
@@ -29,6 +34,10 @@ export default class Calculator {
      * @param {number} b - a második szám.
      * @returns {number} a két paraméterként kapott szám különbsége.
      */
+
+     difference(a: number, b:number){
+        return a-b;
+    }
 
     
     /** 
@@ -41,6 +50,11 @@ export default class Calculator {
      * @param {User} user - az objektum.
      * @returns {any} a kulcs alapján kiolvasott tulajdonság értéke.
      */
+
+    getProperty(user :{name: string, email: string}){
+        return user.name;
+    }
+
 
 
 }
